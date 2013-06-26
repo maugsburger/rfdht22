b79e3b5b453a061f25a38859376d8080199c8bfd
Moritz Augsburger <moritz@augsburger.name>
2013-06-26 08:16:28 +0000

    fileencoding, strict prototype warnings

--- src/lib/spi.h
+++ src/lib/spi.h
@@ -30,7 +30,7 @@
 #include <avr/io.h>
 
 
-extern void spi_init();
+extern void spi_init(void);
 extern void spi_transfer_sync (uint8_t * dataout, uint8_t * datain, uint8_t len);
 extern void spi_transmit_sync (uint8_t * dataout, uint8_t len);
 extern uint8_t spi_fast_shift (uint8_t data);
--- src/lib/wl_module.h
+++ src/lib/wl_module.h
@@ -62,37 +62,37 @@ extern volatile uint8_t PTX;
 #define RX_POWERUP wl_module_config_register(CONFIG, wl_module_CONFIG | ( (1<<PWR_UP) | (1<<PRIM_RX) ) )
 
 // Public standard functions
-extern void wl_module_init();
-extern void wl_module_config();
+extern void wl_module_init(void);
+extern void wl_module_config(void);
 extern void wl_module_send(uint8_t * value, uint8_t len);
 extern void wl_module_set_RADDR(uint8_t * adr);
 extern void wl_module_set_TADDR(uint8_t * adr);
-extern uint8_t wl_module_data_ready();
+extern uint8_t wl_module_data_ready(void);
 //extern void wl_module_get_data(uint8_t * data);
 extern uint8_t wl_module_get_data(uint8_t * data);			//Gibt die Werte des STATUS-Registers zurück
 
 //Public functions
 
-extern uint8_t wl_module_get_status();
-extern uint8_t wl_module_get_rx_pipe_reading_status();
+extern uint8_t wl_module_get_status(void);
+extern uint8_t wl_module_get_rx_pipe_reading_status(void);
 extern uint8_t wl_module_get_one_byte(uint8_t command);
 extern void wl_module_set_rx_pw(unsigned char payloadwidth, unsigned char rxpipenum);
 extern uint8_t wl_module_get_rx_pw(uint8_t rxpipenum);
 extern void wl_module_set_tx_addr(uint8_t * address, uint8_t len);
 extern void wl_module_set_rx_addr(uint8_t * address, uint8_t len, uint8_t rxpipenum);
 extern void wl_module_tx_config(uint8_t tx_nr);
-extern void wl_module_rx_config();
+extern void wl_module_rx_config(void);
 extern void wl_module_get_rx_addr(uint8_t * data, uint8_t rxpipenum, uint8_t len);
-extern uint8_t wl_module_get_rx_pipe();
+extern uint8_t wl_module_get_rx_pipe(void);
 extern uint8_t wl_module_get_rx_pipe_from_status(uint8_t status);
-extern uint8_t wl_module_fifo_tx_empty();	//returns true if TX_EMPTY bit in FIFO_STATUS register is set, false otherwise
-extern uint8_t wl_module_fifo_rx_empty();
-extern uint8_t wl_module_get_rf_ch();
-extern uint8_t wl_module_get_rf_setup();
-extern uint8_t wl_module_get_plos_cnt();
-extern uint8_t wl_module_get_arc_cnt();
-extern void wl_module_set_as_tx();			//activate module with existing config
-extern void wl_module_power_down();			//powers down the module with existing config
+extern uint8_t wl_module_fifo_tx_empty(void);	//returns true if TX_EMPTY bit in FIFO_STATUS register is set, false otherwise
+extern uint8_t wl_module_fifo_rx_empty(void);
+extern uint8_t wl_module_get_rf_ch(void);
+extern uint8_t wl_module_get_rf_setup(void);
+extern uint8_t wl_module_get_plos_cnt(void);
+extern uint8_t wl_module_get_arc_cnt(void);
+extern void wl_module_set_as_tx(void);			//activate module with existing config
+extern void wl_module_power_down(void);			//powers down the module with existing config
 
 // Public extended functions
 extern void wl_module_config_register(uint8_t reg, uint8_t value);
