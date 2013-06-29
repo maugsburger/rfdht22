caed812ec5633567acea0d8c634f5fa3b49cbec9
Moritz Augsburger <moritz@augsburger.name>
2013-06-29 21:35:57 +0000

    successfully sending

--- src/sensor/main.c
+++ src/sensor/main.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 #include <avr/io.h>
 #include <util/delay.h>
-#include <avr/interrupt.h> 
+#include <avr/interrupt.h>
+#include "nRF24L01.h"
+#include "spi.h"
 #include "wl_module.h"
-#include "uart.h"
 #include "dht22.h"
-
 
 void dhtpwr( bool on ) {
     if( on ) {
@@ -15,61 +15,102 @@ void dhtpwr( bool on ) {
         TCCR0A =    (1<<WGM01) | (1<<WGM00) | //
                     (1<<COM0A1); // Clear OC0A on match, set at TOP
         TCCR0B = (0<<CS01) | (1<<CS00); // no prescaling
-        OCR0A = 127; 
+        OCR0A = 127;
 
         // PB1 VCC für Doubler
         PORTB |= (1<<PB1);
         // PB2 und PB1 Ausgang
-        DDRB |= (1<<PB2) | (1<<PB1); 
+        DDRB |= (1<<PB2) | (1<<PB1);
     } else {
         // PB2 und PB1 kein Ausgang
-        DDRB &= ~(1<<PB2) | (1<<PB1); 
+        DDRB &= ~(1<<PB2) | (1<<PB1);
         PORTB &= ~(1<<PB1);
         TCCR0A = (0<<WGM01) | (0<<WGM00);   //normal port operation
         TCCR0B = (0<<CS01) | (0<<CS00);     // stop counter
     }
 }
 
-void dhtpwrt(void) {
-    if( DDRB & (1<<PB2) ) {
-        dhtpwr(false);
-    } else {
-        dhtpwr(true);
-    }
+void blink(uint8_t c) {
+    do {
+        PORTA |= (1<<PA0);
+        _delay_ms(100);
+        PORTA &= ~(1<<PA0);
+        _delay_ms(100);
+        c--;
+    } while (c>0);
+    _delay_ms(500);
 }
 
-
 int main (void) {
-    uart_init();
-    uart_puts("\n\rrestart\n\r");
 
     uint16_t temperature;
     uint16_t humidity;
     uint8_t  dht22res;
 
-    dhtpwr(true);
+    uint8_t payload[wl_module_PAYLOAD];		//Array for Payload
+	uint8_t k;
+	
+	wl_module_init();	    //initialise nRF24L01+ Module
+	_delay_ms(50);		    //wait for nRF24L01+ Module
+    wl_module_tx_config(wl_module_TX_NR_0);		//Config Module
+
+ //   dhtpwr(true);
 
     PORTA = (1<<PA1) | (1<<PA0);
     DDRA  = (1<<PA0) | (1<<PA1);
-    DDRD &= ~(1<<PD6); //PD6 als Eingang
-    _delay_ms(500); 
-
-    char s[7];
+    _delay_ms(500);
 
     while(1) {
+        // measure
         PORTA = (1<<PA1);
         dht22res = dhtmeasure(&temperature, &humidity );
-        uart_puts("return ");
-        uart_puts( utoa( dht22res, s, 10) );
-        uart_puts("\n\r");
-        uart_puts("hum ");
-        uart_puts( utoa( humidity, s, 10) );
-        uart_puts("\n\r");
-        uart_puts("temp ");
-        uart_puts( utoa( temperature, s, 10) );
-        uart_puts("\n\r");
         PORTA = 0;
-        _delay_ms(10000); 
+
+        for (k=0; k<=wl_module_PAYLOAD-1; k++) {
+            payload[k] = k;
+        }
+        payload[0] = dht22res;
+        payload[1] = temperature;
+        payload[2] = (temperature>>8);
+        payload[3] = humidity;
+        payload[4] = (humidity>>8);
+
+        sei();
+        wl_module_send(payload,wl_module_PAYLOAD);
+        _delay_ms(5000);
+
+    }
+}
+
+#if defined(__AVR_ATtiny2313__) || defined(__AVR_ATtiny2313A__)
+ISR(INT0_vect) {
+#endif
+    uint8_t status;
+
+    // Read wl_module status
+    wl_module_CSN_lo;                               // Pull down chip select
+    status = spi_fast_shift(NOP);					// Read status register
+    wl_module_CSN_hi;                               // Pull up chip select
+
+    // Package has been sent
+    if (status & (1<<TX_DS)) {
+        wl_module_config_register(STATUS, (1<<TX_DS));	//Clear Interrupt Bit
+        PTX=0;
+    }
+
+    // IRQ: Package has not been sent, send again
+    if (status & (1<<MAX_RT)) {
+        wl_module_config_register(STATUS, (1<<MAX_RT));	// Clear Interrupt Bit
+        wl_module_CE_hi;								// Start transmission
+        _delay_us(10);								
+        wl_module_CE_lo;
+    }
+
+    //TX_FIFO Full <-- this is not an IRQ
+    if (status & (1<<TX_FULL)) {
+        wl_module_CSN_lo;                               // Pull down chip select
+        spi_fast_shift(FLUSH_TX);						// Flush TX-FIFO
+        wl_module_CSN_hi;                               // Pull up chip select
     }
 }
 /*
